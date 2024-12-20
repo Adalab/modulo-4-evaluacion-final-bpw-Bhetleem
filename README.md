e0bb0ad299f12f0b45a506c94c09e9a4694c6db7
Juande <juande@Juandes-MacBook-Pro-2.local>
2024-12-20 13:23:01 +0100

    Endpoints and README

---
+++ README.md
@@ -0,0 +1,53 @@
+# Aplicación Backend con **Express.js** y **Node.js**
+
+Este repositorio contiene una aplicación backend desarrollada con **Node.js** y el framework **Express.js**. Utiliza **MySQL** como base de datos y tiene cuatro endpoints básicos para realizar operaciones CRUD: agregar, borrar, modificar y leer.
+
+## Requisitos
+
+Antes de ejecutar la aplicación, asegúrate de tener instalados los siguientes programas:
+
+- **Node.js** 
+- **MySQL**
+- **Visual Studio Code** 
+
+## Instalación
+
+Sigue estos pasos para configurar y ejecutar el proyecto en tu entorno local:
+
+### 1. Clonar el repositorio
+
+Clona este repositorio en tu máquina local:
+
+```bash
+git clone https://github.com/Adalab/modulo-4-evaluacion-final-bpw-Bhetleem.git
+2. Instalar dependencias
+Navega al directorio del proyecto y ejecuta:
+
+bash
+npm install
+npm install express
+npm install cors
+npm install mysql2
+npm install dotenv
+npm install nodemon (opcional)
+3. Configurar la base de datos
+Asegúrate de tener MySQL en funcionamiento y crea una base de datos llamada "apariv".
+
+4. Ejecutar el servidor
+Inicia el servidor con:
+
+bash
+npm start o npm run dev (nodemon)
+
+5. Crear el archivo .env con la contraseña de la base de datos.
+
+Endpoints disponibles
+GET /api/dogs: Obtiene todos los registros de la tabla dogs.
+POST /api/dogs: Agrega un nuevo perro a la tabla dogs.
+PATCH /api/dogs/:id: Modifica un registro de la tabla dogs existente.
+DELETE /api/dog/:id: Elimina un registro de la tabla dogs.
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -4,12 +4,101 @@ const mysql = require("mysql2/promise");
 
 const server = express();
 
+require("dotenv").config();
+
 server.use(cors());
+server.use(express.json());
+
+async function getDBConnection() {
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: process.env.PASSWORD_DB,
+        database: "apariv"
+    });
+    connection.connect();
+    return connection;
+}
 
 const port = 4002;
 server.listen(port, () => {
-    console.log(`Server is running. Go to http://localhost:${port}`);
 })
+
+server.post("/api/dogs", async (req, res) => {
+    console.log(req.body);
+    const { name, breed, DOB, behaviour } = req.body;
+    if (!name || !breed || !DOB || !behaviour) {
+        res.status(400).json({
+            success: false,
+            message: "Missing fields. Please fill them"
+         })
+    } else {
+        const connection = await getDBConnection();
+        const query = "INSERT INTO dogs(name, breed, DOB, behaviour) VALUES (?, ?, ?, ?)";
+        const [result] = await connection.query(query, [name, breed, DOB, behaviour]);
+        connection.end();
+        res.status(201).json({
+            success: true,
+            id: result.insertId 
+        });
+    }
+})
+
+server.get("/api/dogs", async (req, res) => {
+    const connection = await getDBConnection();
+    const query = "SELECT * FROM dogs";
+    const [result] = await connection.query(query);
+    connection.end();
+    res.status(200).json({
+        info: { count: result.length },
+        results: result
+    })
+})
+
+server.patch("/api/dogs/:id", async (req, res) => {
+    const id = req.params.id;
+    const { name, breed, DOB, behaviour, fk_adopter } = req.body;
+    const connection = await getDBConnection();
+
+    let query = "UPDATE dogs SET";
+    const params = [];
+
+    if (name){ 
+        query = query + " name = ?,"
+        params.push(name)
+    }
+    if (breed){ 
+        query = query + " breed = ?,"
+        params.push(breed)
+    }
+    if (DOB){ 
+        query = query + " DOB = ?,"
+        params.push(DOB)
+    }
+    if (behaviour){ 
+        query = query + " behaviour = ?,"
+        params.push(behaviour)
+    }
+    if (fk_adopter){ 
+        query = query + " fk_adopter = ?"
+        params.push(fk_adopter)
+    }
+    query = query + " WHERE id = ?;"
+
+    params.push(req.params.id)
+    const [result] = await connection.query(query, params);
+    connection.end();
+    res.status(200).json({ success: true });
+})
+
+server.delete("/api/dog/:id", async (req, res) => {
+    const connection = await getDBConnection();
+    const query = "DELETE from dogs WHERE id = ?";
+    const [result] = await connection.query(query, [req.params.id]);
+    res.status(200).json({ success: true });
+})
+
+
 
 
 
